605a0bf04ce6f055d1ed77cefb35afe69c2eef3d
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2019-06-03 12:44:57 -0400

    removing 3.4

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         ]
     },
     include_package_data=True,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=requirements,
     license='BSD',
     zip_safe=False,
@@ -72,7 +72,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
